92a7154d9af2952b45b466c6dc7081e0e4fe8f4a
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-06 22:26:00 +0200

    Replaced the built-in docs with ddox.

--- source/app.d
+++ source/app.d
@@ -1,16 +1,17 @@
 import vibe.d;
 
 import vibelog.vibelog;
-import api;
 
+import ddox.ddox;
+import ddox.entities;
+import ddox.htmlserver;
+import ddox.jsonparser;
 import std.algorithm;
 import std.array;
 import std.datetime;
 
 
-string[] s_moduleNames;
-Json[string] s_modules;
-Json[] s_projectTree;
+Package m_rootPackage;
 
 void download(HttpServerRequest req, HttpServerResponse res)
 {
@@ -18,105 +19,6 @@ void download(HttpServerRequest req, HttpServerResponse res)
 		res.redirect("https://github.com/rejectedsoftware/vibe.d/"~req.query["file"]);
 	else res.renderCompat!("download.dt", HttpServerRequest, "req")(Variant(req));
 }
-
-void showApi(HttpServerRequest req, HttpServerResponse res)
-{
-	struct Info2 {
-		string[] moduleNames;
-		Json[string] modules;
-		Json[] projectTree;
-	}
-	Info2 info;
-
-	info.moduleNames = s_moduleNames;
-	info.modules = s_modules;
-	info.projectTree = s_projectTree;
-
-	res.renderCompat!("api.dt",
-		HttpServerRequest, "req",
-		Info2*, "info")
-		(Variant(req), Variant(&info));
-}
-
-void showApiModule(HttpServerRequest req, HttpServerResponse res)
-{
-	struct Info2 {
-		string[] moduleNames;
-		Json[string] modules;
-		Json[] projectTree;
-		string moduleName;
-	}
-	Info2 info;
-
-	info.moduleName = req.params["modulename"];
-	info.moduleNames = s_moduleNames;
-	info.modules = s_modules;
-	info.projectTree = s_projectTree;
-
-	auto pm = info.moduleName in s_modules;
-	if( pm is null ) return;
-
-	res.renderCompat!("api-module.dt",
-		HttpServerRequest, "req",
-		Info2*, "info")
-		(Variant(req), Variant(&info));
-}
-
-void showApiItem(HttpServerRequest req, HttpServerResponse res)
-{
-	struct Info3 {
-		string[] moduleNames;
-		Json[string] modules;
-		Json[] projectTree;
-		string moduleName;
-		Json item;
-		Json overloads;
-	}
-	Info3 info;
-
-
-	info.moduleName = req.params["modulename"];
-	info.moduleNames = s_moduleNames;
-	info.modules = s_modules;
-	info.projectTree = s_projectTree;
-
-	auto pm = info.moduleName in s_modules;
-	if( pm is null ) return;
-
-	auto itemsteps = req.params["itemname"].split(".");
-	if( itemsteps.length == 0 ) return;
-
-	info.item = *pm;
-	info.overloads = *pm;
-	foreach( i, st; itemsteps ){
-		search:
-		foreach( mcat; info.item.members )
-			foreach( mem; mcat ){
-				auto mcmp = mem.type == Json.Type.Array ? mem[0] : mem;
-				if( mcmp.name == st ){
-					info.item = mcmp;
-					info.overloads = mem;
-					break search;
-				}
-			}
-	}
-
-	switch( info.item.kind.get!string ){
-		default: logWarn("Unknown API item kind: %s", info.item.kind.get!string); return;
-		case "function":
-			res.renderCompat!("api-function.dt", HttpServerRequest, "req", Info3*, "info")(Variant(req), Variant(&info));
-			break;
-		case "interface":
-		case "class":
-		case "struct":
-			res.renderCompat!("api-class.dt", HttpServerRequest, "req", Info3*, "info")(Variant(req), Variant(&info));
-			break;
-		case "enum":
-			res.renderCompat!("api-enum.dt", HttpServerRequest, "req", Info3*, "info")(Variant(req), Variant(&info));
-			break;
-	}
-}
-
 
 void error(HttpServerRequest req, HttpServerResponse res, HttpServerErrorInfo error)
 {
@@ -132,13 +34,9 @@ void updateDocs()
 		import std.file;
 		string text = readText("docs.json");
 		auto json = parseJson(text);
-		//s_projectTree = cast(Json[])json;
-		foreach( m; json ){
-			auto mname = m.name.get!string;
-			s_moduleNames ~= mname;
-			s_modules[mname] = m;
-		}
-		s_moduleNames.sort();
+		auto settings = new DdoxSettings;
+		m_rootPackage = parseJsonDocs(json, settings);
+		//m_rootPackage = loaDdox(json);
 	} catch( Exception e ){
 		logError("Error loading docs: %s", e.toString());
 		throw e;
@@ -177,11 +75,8 @@ static this()
 	router.get("/templates", staticRedirect("/templates/"));
 	router.get("/templates/", staticRedirect("/templates/diet"));
 	router.get("/templates/diet", staticTemplate!"templates.dt");
-	router.get("/api", staticRedirect("/api/"));
-	router.get("/api/", &showApi);
-	router.get("/api/:modulename", delegate(req, res){ res.redirect("/api/"~req.params["modulename"]~"/"); });
-	router.get("/api/:modulename/", &showApiModule);
-	router.get("/api/:modulename/:itemname", &showApiItem);
+
+	registerApiDocs(router, m_rootPackage, "/api", false);
 
 	auto blogsettings = new VibeLogSettings;
 	blogsettings.configName = "vibe.d";
